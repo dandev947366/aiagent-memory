e1ff9b2897b23472b2f3903e976b48998ad6373b
danofoundation <dan.dev947366@gmail.com>
2024-08-11 12:53:46 +0300

    Add create vectordb

--- assistant.py
+++ assistant.py
@@ -1,3 +1,4 @@
+import chromadb
 import ollama
 
 # First pull llama
@@ -10,6 +11,17 @@ import ollama
 
 # print(response)
 
+client = chromadb.Client()
+
+message_history = [
+    {"id": 1, "prompt": "What is my name?", "response": "Your name is Dan Le."},
+    {"id": 2, "prompt": "What is square root of 9876?", "response": "99.3780659904"},
+    {
+        "id": 3,
+        "prompt": "What kind of dog do I have?",
+        "response": "Your dog's name is Roxy.",
+    },
+]
 convo = []
 
 
@@ -28,6 +40,23 @@ def stream_response(prompt):
     convo.append({"role": "assistant", "content": prompt})
 
 
+def create_vector_db(conversations):
+    vector_db_name = "conversations"
+    try:
+        client.delete_collection(name=vector_db_name)
+    except ValueError:
+        pass
+    vector_db = client.create_collection(name=vector_db_name)
+    for c in conversations:
+        serialized_convo = f"prompt: {c['prompt']} response: {c['response']}"
+        response = ollama.embeddings(model="nomic-embed-text", prompt=serialized_convo)
+        embedding = response["embedding"]
+        vector_db.add(
+            ids=[str(c["id"])], embeddings=[embedding], documents=[serialized_convo]
+        )
+
+
+create_vector_db(conversations=message_history)
 while True:
     prompt = input("USER: \n")
     stream_response(prompt=prompt)
